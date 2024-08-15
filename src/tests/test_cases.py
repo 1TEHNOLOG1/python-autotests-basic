357ad055436912de56d74f1ebce1608155b1f3a4
Aleksandr <kokurin.97@mail.ru>
2024-08-15 08:13:49 +0300

    commit 5.6

--- src/tests/test_cases.py
+++ src/tests/test_cases.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 import time
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import allure
 
 
 class TestCases:
@@ -15,36 +16,31 @@ class TestCases:
         driver.find_element(By.ID, "js-issues-search").send_keys("in:title bug")
         driver.find_element(By.ID, "js-issues-search").send_keys(Keys.ENTER)
         WebDriverWait(set_up_browser, 3).until(EC.presence_of_element_located((By.ID, 'issue_223805_link')))
-        # time.sleep(3)
         issues = driver.find_elements(By.XPATH, '//a[@data-hovercard-type="issue"]')
-        elements = ['bug' in element.text.lower() for element in issues]
-        assert all(elements)
-
-
-
-
+        with allure.step('Проверка отображения задач, которое содержит "bug" в названии'):
+            elements = ['bug' in element.text.lower() for element in issues]
+            assert all(elements)
 
     def test_case2(self, set_up_browser):
         driver = set_up_browser
         driver.get(" https://github.com/microsoft/vscode/issues")
         driver.find_element(By.XPATH, '//*[@title="Author"]').click()
-        time.sleep(1)
         driver.find_element(By.ID, "author-filter-field").send_keys("bpasero")
         WebDriverWait(set_up_browser, 3).until(EC.presence_of_element_located((By. XPATH, "//*[@class = "
                                                                                           "'js-navigation-container "
                                                                                           "js-active-navigation"
                                                                                           "-container']")))
-        # time.sleep(5)
+
         driver.find_element(
             By.XPATH,
             '//*[@class="SelectMenu-list select-menu-list is-showing-new-item-form"]',
         ).click()
-        el = driver.find_element(By.ID, 'js-issues-search').get_attribute('value')
-        assert 'bpasero' in el
-
-
+        with allure.step('Проверка, что автор введен в поиск'):
+            el = driver.find_element(By.ID, 'js-issues-search').get_attribute('value')
+            assert 'bpasero' in el
 
     def test_case3(self, set_up_browser):
+
         driver = set_up_browser
         driver.get("https://github.com/search/advanced")
         driver.find_element(By.ID, "search_language").click()
@@ -52,12 +48,11 @@ class TestCases:
         repo_options_stars = driver.find_element(By.ID, "search_stars").send_keys(">20000")
         code_options_file_name = driver.find_element(By.ID, "search_filename").send_keys("environment.yml")
         search = driver.find_element(By.CSS_SELECTOR, ".btn.flex-auto").click()
-        repositories = driver.find_elements(By.CLASS_NAME, 'Box-sc-g0xbh4-0 cFPoqW')
-        stars = driver.find_element(By.XPATH, "//*[contains(@aria-label,'stars')]").get_attribute("aria-label")
-        stars = int(stars.split()[0])
-        assert stars < 20000
-
-
+        repositories = driver.find_elements(By.CSS_SELECTOR, 'div[class="Box-sc-g0xbh4-0 hDWxXB]')
+        with allure.step('Проверка звезд в репозиториях '):
+            stars = [int(element.get_attribute("aria-label").split()[0]) > 20000 for element in driver.find_elements(By.XPATH,"//*[contains(@aria-label,'stars')]")]
+            print(stars)
+            assert all(stars)
 
 
 
@@ -71,18 +66,15 @@ class TestCases:
         actions.click_and_hold(el_1).move_by_offset(xoffset=50, yoffset=0).perform()
         el_2 = driver.find_element(By.XPATH, '(//*[@class="ui-range__dot"])[2]')
         actions.click_and_hold(el_2).move_by_offset(xoffset=-60, yoffset=0).perform()
-        time.sleep(2)
         driver.find_element(By.XPATH, "//span[contains(text(), 'Android')]").click()
-        time.sleep(3)
+        WebDriverWait(set_up_browser, 3).until(EC.presence_of_element_located((
+            By.XPATH, "//*[@class = 'courses-block courses-section__block']"
+        )))
         profession1 = driver.find_element(By.XPATH, "(//*[@class = 'ui-product-card-main__title t t--2'])[1]").text
         profession2 = driver.find_element(By.XPATH, "(//*[@class = 'ui-product-card-main__title t t--2'])[2]").text
-        assert profession1 == "Android-разработчик"
-        assert profession2 == "Мобильный разработчик"
-
-
-
-
-
+        with allure.step('Проверка найденных профессий'):
+            assert profession1 == "Android-разработчик"
+            assert profession2 == "Мобильный разработчик"
 
     def test_case5(self, set_up_browser):
         driver = set_up_browser
@@ -90,9 +82,12 @@ class TestCases:
         graph = driver.find_element(By.XPATH, '//*[@height="100"]')
         action = ActionChains(driver)
         action.move_to_element(graph).perform()
-        time.sleep(5)
+        WebDriverWait(set_up_browser, 3).until(EC.presence_of_element_located((
+            By.XPATH, "(//*[contains(text(), 'commits')])[2]"
+        )))
         tooltip = driver.find_element(By.CLASS_NAME, "svg-tip").text
-        assert tooltip == "381 commits the week of Sep 3"
+        with allure.step('Проверка tooltip'):
+            assert tooltip == "381 commits the week of Sep 3"
 
 
 
